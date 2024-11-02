bb25cdc1239ba916359e01559de3266455baa3cc
ira <irynaira@gmail.com>
2024-11-02 18:07:18 +0200

    db = postgresql

--- cinema/settings.py
+++ cinema/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'cinema.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),          # Database name
+        'USER': os.getenv('DB_USER'),           # Database username
+        'PASSWORD': os.getenv('DB_PASSWORD'),   # Database password
+        'HOST': os.getenv('DB_HOST', 'localhost'),  # Database hostname (localhost by default)
+        'PORT': os.getenv('DB_PORT', '5432'),   # Database port (5432 is default for PostgreSQL)
     }
 }
 
